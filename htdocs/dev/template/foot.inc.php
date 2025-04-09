34ca5b8b73d507ddae589df07be052d684fd7ad3
Dracosomething <larsfalk08@gmail.com>
2025-04-09 14:10:36 +0200

    worked on mockup

--- htdocs/dev/template/foot.inc.php
+++ htdocs/dev/template/foot.inc.php
@@ -1,4 +1,13 @@
-<script src="js/uikit.min.js"></script>
+<footer class="uk-container">
+        <div class="uk-overlay-primary uk-position-bottom">
+            <div>
+                <br>
+                <p>telefoon: 12345677890</p>
+                <p>mail: stoelensleepers_bussiness@sleepers-mail.com</p>
+            </div>
+        </div>
+    </footer>
+    <script src="js/uikit.min.js"></script>
     <script src="js/uikit-icons.min.js"></script>
 </body>
 
