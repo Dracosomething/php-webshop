2cb0b944fad60317bcd0e77c3d8fa2f467a2a27d
Dracosomething <larsfalk08@gmail.com>
2025-04-10 11:34:53 +0200

    worked on wireframes

--- htdocs/dev/template/foot.inc.php
+++ htdocs/dev/template/foot.inc.php
@@ -1,9 +1,11 @@
-<footer class="uk-container">
-        <div class="uk-overlay-primary uk-position-bottom">
-            <div>
-                <br>
-                <p>telefoon: 12345677890</p>
-                <p>mail: stoelensleepers_bussiness@sleepers-mail.com</p>
+<footer class="uk-margin-xlarge-top">
+        <div uk-sticky>
+            <div class="uk-overlay-primary uk-position-bottom uk-container">
+                <div>
+                    <br>
+                    <p>telefoon: 12345677890</p>
+                    <p>mail: stoelensleepers_bussiness@sleepers-mail.com</p>
+                </div>
             </div>
         </div>
     </footer>
