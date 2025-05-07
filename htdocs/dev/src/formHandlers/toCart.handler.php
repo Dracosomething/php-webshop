22346532cec00c737104ad7e6e6714b30874b31d
MC-Alblas <mc.alblas0207@gmail.com>
2025-05-07 16:28:46 +0200

    toCart handler progress

--- htdocs/dev/src/formHandlers/toCart.handler.php
+++ htdocs/dev/src/formHandlers/toCart.handler.php
@@ -5,7 +5,7 @@ include_once(__DIR__ . "/../helpers/array.helper.php");
 
 try {
     $ArrayHelper = new ArrayHelper();
-    $dbconn = new Database(); // conects to the database
+    $dbconn = new Database(); // connects to the database
 
     if ($ArrayHelper->anyNotSetOrEmpty($_POST)) { // checks if any of the variables are set
         $errorMsg = "value was invalid";
@@ -13,7 +13,8 @@ try {
         exit(); // stops the rest of the code from running
     }
 
-    
+    $Amount = $_POST['amount'];
+    $ProductId = $_POST['productId'];
 
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
