608619ce18f23a71bc8dac37b5537b4d2c4db9a3
Dracosomething <larsfalk08@gmail.com>
2025-05-08 11:01:38 +0200

    added getCard method

--- htdocs/dev/src/helpers/card.helper.php
+++ htdocs/dev/src/helpers/card.helper.php
@@ -1,12 +1,23 @@
 <?php
 include_once(__DIR__ . "/../database/Database.class.php");
+include_once(__DIR__ . "/login.helper.php");
 
 class CardHelper {
     private Database $dbconn;
+    private LoginHelper $login;
 
     public function __construct(Database $dbconn) {
         $this->dbconn = $dbconn;
+        $this->login = new LoginHelper();
     }
 
-    
+    public function getCard(): array {
+        $userID = $this->login->getUser()["ID"];
+        $card = $this->dbconn->select("carts", ["*"], ["customer_id = :CustomerID"], [":CustomerID" => $userID]);
+        return $card;
+    }
+
+    public function doesCardExist() {
+        
+    }
 }
