c4b47ec0141600b9cd9e52083286f7d51c2b61de
MC-Alblas <mc.alblas0207@gmail.com>
2025-05-08 14:18:18 +0200

    bug fixes and testing

--- htdocs/dev/src/formHandlers/toCart.handler.php
+++ htdocs/dev/src/formHandlers/toCart.handler.php
@@ -2,13 +2,14 @@
 include_once(__DIR__ . "/../database/Database.class.php");
 include_once(__DIR__ . "/../helpers/array.helper.php");
 include_once(__DIR__ . "/../helpers/cart.helper.php");
-
-echo "hello world";
+include_once(__DIR__ . "/../helpers/login.helper.php");
 
 try {
     $ArrayHelper = new ArrayHelper();
     $dbconn = new Database(); // connects to the database
     $CartHelper = new CartHelper($dbconn);
+    $login = new LoginHelper();
+
 
     if ($ArrayHelper->anyNotSetOrEmpty($_POST)) { // checks if any of the variables are set
         $errorMsg = "value was invalid";
@@ -20,14 +21,25 @@ try {
     $ProductID = $_POST['productId'];
     $OrderID = $CartHelper->getCartID();
 
-    $CartItem = $dbconn->select("'cart_items'", ["*"], ["'product_id' = :ProductId", "'order_id' = :OrderId"], [
-        ":ProductId" => $ProductId,
+    if ($CartHelper->doesCartExist()) {
+        $userID = $login->getUser()["ID"];
+
+        $dbconn->insert(
+            "carts",
+            [
+                "customer_id" => $userID
+            ]
+        );
+    }
+
+    $CartItem = $dbconn->select("cart_items", ["*"], ["product_id = :ProductId", "order_id = :OrderId"], [
+        ":ProductId" => $ProductID,
         ":OrderId" => $OrderID
     ]);
 
     if (empty($CartItem) || is_null($CartItem)) 
     {
-        $dbconn->insert("'cart_items'", 
+        $dbconn->insert("cart_items", 
             [
             "order_id" => ":OrderId",
             "product_id" => ":ProductId",
@@ -44,6 +56,7 @@ try {
          ]);
     }
 
+    header("Location: ../../product.php?product_id=$ProductID");
 } catch (PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
